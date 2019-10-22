04686df1b292210a97a613a49efc4f7e230a5416
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-10-22 14:28:26 +0100

    Person class created and tested

---
+++ specs/person_spec.rb
@@ -0,0 +1,22 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../person.rb')
+
+class PersonTest < MiniTest::Test
+
+def test_get_name
+  person = Person.new("James", 25)
+  assert_equal("James", person.name)
+end
+
+def test_get_age
+  person = Person.new("James", 25)
+  assert_equal(25, person.age)
+end
+
+
+
+
+
+
+end
